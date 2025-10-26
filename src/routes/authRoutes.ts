fb2ef3bffd8a32ac1bf6c095e4fde0f1bd616a71
ramireddy <143273541+RamiReddy-G@users.noreply.github.com>
2025-10-26 17:49:16 +0530

    Updated authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,15 +4,16 @@ import {
   verifySignup,
   login,
   requestForgotPasswordOTP,
-  verifyForgotPasswordOTP
+  verifyForgotPasswordOTP,
+  updateDeviceToken
 } from '../controllers/authController';
-
+import { requireAuth } from '../middleware/authMiddleware';
 const router = express.Router();
 
 router.post('/signup/request-otp', signupRequestOTP);
 router.post('/signup/verify', verifySignup);
 router.post('/login', login);
-
+router.put('/device-token', requireAuth, updateDeviceToken);
 router.post('/forgot/request-otp', requestForgotPasswordOTP);
 router.post('/forgot/verify', verifyForgotPasswordOTP);
 
