fbe56902dad5162d3c6cbed38a368ccfc46fb6e1
ramireddy <143273541+RamiReddy-G@users.noreply.github.com>
2025-10-27 15:59:54 +0530

    added profile route in authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,13 +5,15 @@ import {
   login,
   requestForgotPasswordOTP,
   verifyForgotPasswordOTP,
-  updateDeviceToken
+  updateDeviceToken,
+  getProfile
 } from '../controllers/authController';
 import { requireAuth } from '../middleware/authMiddleware';
 const router = express.Router();
 
 router.post('/signup/request-otp', signupRequestOTP);
 router.post('/signup/verify', verifySignup);
+router.get('/profile', requireAuth, getProfile);
 router.post('/login', login);
 router.put('/device-token', requireAuth, updateDeviceToken);
 router.post('/forgot/request-otp', requestForgotPasswordOTP);
